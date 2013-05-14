All matrices working properly, need to fix current calculation

matricizer.py
@@ -22,7 +22,7 @@ class Matricizer:
 
         # Compute diffusion coupling coefficients for CMFD
         self.D_tilde = np.zeros(2)
-        self.computeDTildes()
+        self.computeDTilde()
 
         # Create numpy arrays for CMFD destruction (M) and production (F) arrays
         self.M = np.zeros((4,4))
@@ -33,7 +33,8 @@ class Matricizer:
         self.NEM2_coeffs = np.zeros((8,8))
 
 
-    def computeDTildes(self):
+    def computeDTilde(self):
+
         # Diffusion coupling coefficients
         self.D_tilde[0] = (2. * self.D['fuel'][0] * self.D['coolant'][0]) / \
                     (self.delta_x * (self.D['fuel'][0] + self.D['coolant'][0]))
@@ -42,27 +43,27 @@ class Matricizer:
                     (self.delta_x * (self.D['fuel'][1] + self.D['coolant'][1]))
 
 
-    def initializeCMFDDestructionMatrix(self, D_hat=[0., 0.]):
+    def initializeCMFDDestructionMatrix(self, D_hat=np.array([[0.,0.],[0.,0.]])):
 
         # Fuel group 1
-        self.M[0,0] = self.D_tilde[0] - D_hat[0] + (self.abs['fuel'][0] + \
+        self.M[0,0] = self.D_tilde[0] - D_hat[0,0] + (self.abs['fuel'][0] + \
                       self.scatter['fuel']) * self.delta_x
-        self.M[0,2] = -self.D_tilde[0] - D_hat[0]
+        self.M[0,2] = -self.D_tilde[0] - D_hat[0,0]
 
         # Fuel group 2
-        self.M[1,1] = self.D_tilde[1] - D_hat[1] + self.abs['fuel'][1] * \
+        self.M[1,1] = self.D_tilde[1] - D_hat[0,1] + self.abs['fuel'][1] * \
                       self.delta_x
-        self.M[1,3] = -self.D_tilde[1] - D_hat[1]
+        self.M[1,3] = -self.D_tilde[1] - D_hat[0,1]
         self.M[1,0] = -self.scatter['fuel'] * self.delta_x
 
         # Coolant group 1
-        self.M[2,0] = -self.D_tilde[0] + D_hat[0]
-        self.M[2,2] = self.D_tilde[0] - D_hat[0] + (self.abs['coolant'][0] + \
+        self.M[2,0] = -self.D_tilde[0] + D_hat[1,0]
+        self.M[2,2] = self.D_tilde[0] + D_hat[1,0] + (self.abs['coolant'][0] + \
                  self.scatter['coolant']) * self.delta_x
 
         # Coolant group 2
-        self.M[3,1] = -self.D_tilde[1] + D_hat[1]
-        self.M[3,3] = self.D_tilde[1] - D_hat[1] + \
+        self.M[3,1] = -self.D_tilde[1] + D_hat[1,1]
+        self.M[3,3] = self.D_tilde[1] + D_hat[1,1] + \
                  self.abs['coolant'][1] * self.delta_x
         self.M[3,2] = -self.scatter['coolant'] * self.delta_x
 
@@ -74,16 +75,22 @@ class Matricizer:
         self.F[0,1] = self.fission['fuel'][1] * self.delta_x
 
 
-    def initializeNEM4thOrderCoeffMatrix(self):
+    def initializeNEM4thOrderCoeffMatrix(self, keff=1.0):
 
         # Build 4th order NEM matrix using coefficients found in Mathematica
-        self.NEM4_coeffs[0,0] = (self.abs['fuel'][0]+self.scatter['fuel']) / 3.
-        self.NEM4_coeffs[0,2] = (12. * self.D['fuel'][0] / self.delta_x**2) + \
-                            ((self.abs['fuel'][0] + self.scatter['fuel']) / 5.)
+        self.NEM4_coeffs[0,0] = (-self.fission['fuel'][0] / (3. * keff)) + \
+                                (self.abs['fuel'][0]+self.scatter['fuel']) / 3.
+        self.NEM4_coeffs[0,2] = (12. * self.D['fuel'][0] / self.delta_x**2) - \
+                              (self.fission['fuel'][0] / (5. * float(keff))) + \
+                              ((self.abs['fuel'][0] + self.scatter['fuel']) / 5.)
+        print 'keff = ' + str(keff)
+        print 'fission 1 = ' + str(-self.fission['fuel'][1] / 3.)
+        self.NEM4_coeffs[0,4] = -self.fission['fuel'][1] / (3. * keff)
+        self.NEM4_coeffs[0,6] = -self.fission['fuel'][1] / (5. * keff)
 
         self.NEM4_coeffs[1,0] = -self.scatter['fuel'] / 3.
         self.NEM4_coeffs[1,2] = -self.scatter['fuel'] / 5.
-        self.NEM4_coeffs[1,4] = self.abs['fuel'][0] / 3.
+        self.NEM4_coeffs[1,4] = self.abs['fuel'][1] / 3.
         self.NEM4_coeffs[1,6] = (12. * self.D['fuel'][1] / self.delta_x**2) + \
                                 (self.abs['fuel'][1] / 5.)
 
@@ -99,10 +106,14 @@ class Matricizer:
         self.NEM4_coeffs[3,14] = (12. * self.D['coolant'][1] / \
                                 self.delta_x**2) + (self.abs['coolant'][1] / 5.)
 
-        self.NEM4_coeffs[4,1] = (self.abs['fuel'][0]+self.scatter['fuel']) / 5.
-        self.NEM4_coeffs[4,3] = (-12. * self.D['fuel'][0] / self.delta_x**2) - \
-                                           (3. * (self.abs['fuel'][0] + \
-                                           self.scatter['fuel']) / 35.)
+        self.NEM4_coeffs[4,1] = (-self.fission['fuel'][0] / (5. * keff)) + \
+                                (self.abs['fuel'][0]+self.scatter['fuel']) / 5.
+        self.NEM4_coeffs[4,3] = (-12. * self.D['fuel'][0] / self.delta_x**2) + \
+                               (3. * self.fission['fuel'][0] / (35. * keff)) - \
+                               (3. * (self.abs['fuel'][0] + \
+                                      self.scatter['fuel']) / 35.)
+        self.NEM4_coeffs[4,5] = -self.fission['fuel'][1] / (5. * keff)
+        self.NEM4_coeffs[4,7] = 3. * self.fission['fuel'][1] / (35. * keff)
 
         self.NEM4_coeffs[5,1] = -self.scatter['fuel'] / 5.
         self.NEM4_coeffs[5,3] = 3. * self.scatter['fuel'] / 35.
@@ -213,6 +224,9 @@ class Matricizer:
     ############################################################################
     #############################  GETTERS / SETTERS  ##########################
     ############################################################################
+
+    def getMaterializer(self):
+        return self.materializer
 
     def getDiffusionTildes(self):
         return self.D_tilde

solver.py
@@ -1,5 +1,6 @@
 from materializer import *
 from matricizer import *
+from nembasis import *
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.linalg
@@ -11,8 +12,11 @@ class Solver:
 
     def __init__(self, matricizer):
 
-        # Initialize a materializer and matricizer
         self.matricizer = matricizer
+        self.materializer = self.matricizer.getMaterializer()
+
+        # Initialize an NEM basis object
+        self.nem_basis = NEMBasis()
 
         # Initialize keff for each method
         self.cmfd_keff = 1.0
@@ -21,19 +25,29 @@ class Solver:
 
         # Initialize arrays for the x-axis
         self.num_pts = 1000
-        self.x = np.linspace(0., 20., self.num_pts)
-        self.fuel_indices = self.x <= 10.
-        self.coolant_indices = self.x > 10.
+        self.cmfd_x = [0., 5., 15., 20.]
+        self.nem_x = np.linspace(0., 20., self.num_pts)
 
-        # Initialize arrays for fluxes for each method
-        self.cmfd_phi = np.zeros(self.num_pts)
-        self.nem4_phi = np.zeros(self.num_pts)
-        self.nem2_phi = np.zeros(self.num_pts)
+        # Initialize arrays for fluxes (both energy groups) for each method
+        self.cmfd_phi = np.zeros((2, 4))
+        self.nem4_phi = np.zeros((2, self.num_pts))
+        self.nem2_phi = np.zeros((2, self.num_pts))
+
+        # Initialize arrays for NEM 
+        self.nem4_a = np.zeros(16)
+        self.nem2_a = np.zeros(8)
+        self.nem4_phi_bar = np.zeros(16)
+        self.nem2_phi_bar = np.zeros(8)
+        self.D_hat = np.array([[0.,0.],[0.,0.]])
 
 
-    def solveCMFD(self, D_hat=[0.,0.]):
+    ############################################################################
+    #########################  SOLVER CMFD AND NEM ROUTINES ####################
+    ############################################################################
 
-        self.matricizer.initializeCMFDDestructionMatrix(D_hat)
+    def solveCMFD(self, tol=1E-10):
+
+        self.matricizer.initializeCMFDDestructionMatrix(self.D_hat)
         self.matricizer.initializeCMFDProductionMatrix()
 
         M = self.matricizer.getCMFDDestructionMatrix()
@@ -64,12 +78,198 @@ class Solver:
             # Compute residuals
             res = math.sqrt(scipy.linalg.norm(source_old - source_new) / M.size)
 
-            print ("CMFD: i=%d\tres=%1.5E\tkeff=%1.5f" % (i, res, keff))
+            print ("CMFD: i=%d\tres=%1.5E\t\tkeff=%1.15f" % (i, res, keff))
 
             # Check convergence
-            if res < 1E-5:
+            if res < tol:
                 print ("CMFD converged in %d iters with res=%1E"% (i, res))
                 self.cmfd_keff = keff
-                self.cmfd_phi[self.fuel_indices] = phi[0]
-                self.cmfd_phi[self.coolant_indices] = phi[1]
+                self.cmfd_phi[0, 0] = phi[0]      # fuel group 1
+                self.cmfd_phi[0, 1] = phi[0]      # fuel group 1
+                self.cmfd_phi[1, 0] = phi[1]      # fuel group 2
+                self.cmfd_phi[1, 1] = phi[1]      # fuel group 2
+                self.cmfd_phi[0, 2] = phi[2]      # coolant group 1
+                self.cmfd_phi[0, 3] = phi[2]      # coolant group 1
+                self.cmfd_phi[1, 2] = phi[3]      # coolant group 2
+                self.cmfd_phi[1, 3] = phi[3]      # coolant group 2
+                print 'cmfd phi = ' + str(self.cmfd_phi)
                 break
+
+
+    def solveNEM4(self, tol=1E-10):
+    
+        # Solve CMFD without diffusion correction terms
+        self.solveCMFD(tol=tol)
+
+        self.matricizer.initializeNEM4thOrderCoeffMatrix(keff=self.cmfd_keff)
+        nem4_coeff_matrix = self.matricizer.getNEM4thOrderCoeffMatrix()
+
+#        print 'nem4_coeff_matrix1 = ' + str(nem4_coeff_matrix[8:,:])
+#        print nem4_coeff_matrix
+
+        self.nem4_phi_bar[14] = -self.cmfd_phi[0,0] + self.cmfd_phi[0,-1]
+        self.nem4_phi_bar[15] = -self.cmfd_phi[1,0] + self.cmfd_phi[1,-1] 
+
+        print 'phi_bar = ' + str(self.nem4_phi_bar)
+
+        print 'cmfd_phi = ' + str(self.cmfd_phi)
+
+        self.nem4_a = scipy.linalg.solve(nem4_coeff_matrix, self.nem4_phi_bar)
+
+        print 'self.nem4_a = ' + str(self.nem4_a)
+
+        self.nem4_phi = [self.getNEM4Phi(group=1, x=self.nem_x),
+                     self.getNEM4Phi(group=2, x=self.nem_x)]
+        self.plotNEM4Flux(suffix='')
+
+#        print 'phi = ' + str(self.nem4_phi)
+
+        # Compute current at interface in both groups
+        fuel_curr = [self.getNEM4Current(group=1, x=10.), \
+                     self.getNEM4Current(group=2, x=10.)]
+        mod_curr = [self.getNEM4Current(group=1, x=10.), \
+                     self.getNEM4Current(group=2, x=10.)]
+
+        print 'fuel current = ' + str(fuel_curr)
+        print 'moderator current = ' + str(mod_curr)
+
+        # Back out D_hat from interface currents
+        self.D_hat[0][0] = fuel_curr[0] 
+
+        # Solve CMFD with new D_hat
+        
+        # Convergence criterion?
+
+        
+
+
+    def solveNEM2(self, tol=1E-10):
+
+        self.matricizer.initializeNEM2ndOrderCoeffMatrix(self)
+        nem2_coeffs = self.matricizer.getNEM2ndOrderCoeffMatrix(self)
+
+
+    def getNEM4Phi(self, group=1, x=0.):
+
+        if not isinstance(x, np.ndarray):
+            x = np.array([x])
+
+        if x > 10.:
+            x -= 10.
+
+        xi = x/10.
+
+        group -= 1
+        region = (xi / (10.+1E-10)).astype(np.int32)
+
+        phi = self.cmfd_phi[group, region * 2]
+        phi += self.nem4_a[region*8+group*4] * self.nem_basis.P1(xi)
+        phi += self.nem4_a[region*8+group*4+1] * self.nem_basis.P2(xi)
+        phi += self.nem4_a[region*8+group*4+2] * self.nem_basis.P3(xi)
+        phi += self.nem4_a[region*8+group*4+3] * self.nem_basis.P4(xi)
+
+        return phi
+
+
+    def getNEM2Phi(self, group=1, x=0.):
+
+        if not isinstance(x, np.ndarray):
+            x = np.array([x])
+
+        if x > 10.:
+            x -= 10.
+
+        xi = x/10.
+
+        group -= 1
+        region = (xi / (10.+1E-10)).astype(np.int32)
+
+        phi = self.cmfd_phi[group, region * 2]
+        phi += self.nem4_a[region*8+group*4] * self.nem_basis.P1(xi)
+        phi += self.nem4_a[region*8+group*4+1] * self.nem_basis.P2(xi)
+
+        return phi
+
+
+    def getNEM4Current(self, group=1, x=0.):
+
+        if not isinstance(x, np.ndarray):
+            if x > 10.:
+                x -= 10.
+            x = np.array([x])
+
+        xi = x/10.
+
+        print 'xi = ' + str(xi)
+
+        group -= 1
+        region = (xi / (10.+1E-10)).astype(np.int32)
+
+        curr = self.nem4_a[region*8+group*4] * self.nem_basis.DP1(xi)
+        curr += self.nem4_a[region*8+group*4+1] * self.nem_basis.DP2(xi)
+        curr += self.nem4_a[region*8+group*4+2] * self.nem_basis.DP3(xi)
+        curr += self.nem4_a[region*8+group*4+3] * self.nem_basis.DP4(xi)
+
+        if region == 0:
+            curr *= -self.materializer.getDiffusionCoeffs()['fuel'][group]
+        else:
+            curr *= -self.materializer.getDiffusionCoeffs()['coolant'][group]
+
+        return curr[0]
+
+
+    def getNEM2Current(self, group=1, x=0.):
+
+        if not isinstance(x, np.ndarray):
+            x = np.array([x])
+
+        if x > 10.:
+            x -= 10. 
+
+        xi = x/10.
+
+        group -= 1
+        region = (xi / (10.+1E-10)).astype(np.int32)
+
+        curr = self.nem4_a[region*8+group*4] * self.nem_basis.DP1(xi)
+        curr += self.nem4_a[region*8+group*4+1] * self.nem_basis.DP2(xi)
+
+        if region == 0:
+            curr *= -self.materializer.getDiffusionCoeffs()['fuel'][group]
+        else:
+            curr *= -self.materializer.getDiffusionCoeffs()['coolant'][group]
+
+        return curr[0]
+
+
+    ############################################################################
+    ##################################  PLOTTING  ##############################
+    ############################################################################
+
+    def plotCMFDFlux(self):
+
+        fig = plt.figure()
+        plt.plot(self.cmfd_x, self.cmfd_phi[0, :], linewidth=2)
+        plt.plot(self.cmfd_x, self.cmfd_phi[1, :], linewidth=2)
+        plt.title('Normalized CMFD Flux')
+        plt.xlabel('x [cm]')
+        plt.ylabel('Flux')
+        plt.legend(['Group 1', 'Group 2'])
+        plt.grid()
+        plt.savefig('cmfd-flux.png')
+
+    def plotNEM4Flux(self, suffix=''):
+
+        fig = plt.figure()
+        plt.plot(self.nem_x, self.nem4_phi[0])
+        plt.plot(self.nem_x, self.nem4_phi[1])
+        plt.xlabel('x [cm]')
+        plt.ylabel('Flux')
+        plt.title('Normalized NEM 4th Order Flux')
+        plt.legend(['Group 1', 'Group 2'])
+        plt.grid()
+        plt.savefig('cmfd-flux.png')
+
+#    def plotNEM2Flux(self):
+
+#    def plotAllFluxes(self):